a51662fda4b3ac7f21a85f80124b791cff9b675a
Matias Lopez <matiaslopezd@gmail.com>
2020-06-15 22:40:34 -0400

    FIX:Maybe noew yes in firefox?

--- src/index.js
+++ src/index.js
@@ -17,39 +17,37 @@ class RestrictedBrowsers {
     constructor({ compatibility = {}, restrict = true, debug = false }) {
         this.compatibility = compatibility;
         this.restrict = restrict;
-        this.ua = new UAParser();
-        this.browser = { name: '', version: '', os: '' };
+        this.ua = UAParser();
+        this.Browser = { name: '', version: '', os: '' };
         this.debug = debug;
+        this.__init();
     }
 
     async check() {
-      // Get info of browser
-      this.__init();
       // Compare
-      return await this.__compare(this.compatibility, this.browser.name);
+      return await this.__compare(this.compatibility, this.Browser.name);
     }
 
     __init() {
       // Check the restriction object is valid
       if (!this.compatibility || Object.keys(this.compatibility).length < 1) throw new Error('It\'s not possible check compatibility with a empty object.');
       // Get the browser and os
-      const browser = this.ua.getBrowser();
-      const os = this.ua.getOS();
+      const { browser, os } = this.ua;
       // Get browser name parsing first
-      this.browser.name = this.constructor.getName(this.compatibility, browser.name);
+      this.Browser.name = this.constructor.getName(this.compatibility, browser.name);
       // Get OS name parsing first
-      this.browser.os = this.constructor.getName(this.compatibility[this.browser.name], os.name);
+      this.Browser.os = this.constructor.getName(this.compatibility[this.Browser.name], os.name);
       // Get version parsing first
-      this.browser.version = this.constructor.parseVersion(browser.version);
+      this.Browser.version = this.constructor.parseVersion(browser.version);
     }
 
     __compare(obj = {}, name = '') {
       return new Promise(async (resolve) => {
         this.__debug('log', 'Key', name, 'is', typeof obj[name], 'on browsers with value', obj[name]);
         // This check if browser is restricted to version for all os
-        if (name in obj &&  typeof obj[name] === 'string') resolve(satisfies(this.browser.version, obj[name]));
+        if (name in obj &&  typeof obj[name] === 'string') resolve(satisfies(this.Browser.version, obj[name]));
         // This check if browser is restricted to version per os
-        else if (name in obj && typeof obj[name] === 'object') resolve(await this.__compare(obj[name], this.browser.os));
+        else if (name in obj && typeof obj[name] === 'object') resolve(await this.__compare(obj[name], this.Browser.os));
         // If not exist return restriction value
         else resolve(!this.restrict);
       })
